d34edc38b9d67be85b9bcae832673180e558eff4
ThomasDBA-7 <thomas.buenanos272@pascualbravo.edu.co>
2025-03-03 21:16:19 -0500

    First commit

---
+++ ValidacionFestiva/Entidades/Festivo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ValidacionFestiva.Dominio.Entidades
+{
+
+    [Table("Festivo")]
+    public class Festivo
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Dia")]
+        public int Dia { get; set; }
+
+        [Column("Mes")]
+        public int Mes { get; set; }
+
+        [Column("Nombre"), StringLength(100)]
+        public required string Nombre { get; set; }
+
+        [Column("IdTipoFestivo")]
+        public int TipoFestivoId { get; set; }
+
+        [ForeignKey("TipoFestivoId")]
+        public required TipoFestivo TipoFestivo { get; set; }
+
+        [Column("DiasDesdePascua")]
+        public int? DiasDesdePascua { get; set; } 
+    }
+}
---
+++ ValidacionFestiva/Entidades/TipoFestivo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ValidacionFestiva.Dominio.Entidades
+{
+    [Table("TipoFestivo")]
+    public class TipoFestivo
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Descripcion"), StringLength(50)]
+        public required string Descripcion { get; set; }
+
+        [Column("ModoCalculo"), StringLength(50)]
+        public required string ModoCalculo { get; set; }
+    }
+}
